c2f35396a4ec64480d4dd0a18a3aaa9404b16da9
amuthaOlin <ankeet.mutha@students.olin.edu>
2016-01-26 12:11:13 -0500

    Wrote first attempt at writing pwm to motors. Not working. Had to add header files to SConstruct.

--- miniproject1/readAndWritePins.c
+++ miniproject1/readAndWritePins.c
@@ -3,16 +3,26 @@
 #include "common.h"
 #include "ui.h"
 #include "timer.h"
+#include "pin.h"
+#include "oc.h"
 
 int16_t main(void) {
     init_clock();
     init_ui();
     init_timer();
+    init_pin();
+    init_oc();
 
+    float freq = 25000;
+    uint16_t duty = 50;
     led_on(&led1);
     led_off(&led2);
     timer_setPeriod(&timer2, 0.5);
     timer_start(&timer2);
+//    timer_setPeriod(&timer3, 0.5);
+    timer_start(&timer3);
+
+
 
     while (1) {
         if (timer_flag(&timer2)) {
@@ -22,6 +32,8 @@ int16_t main(void) {
         }
 
 
-        led_write(&led3, !sw_read(&sw3));
+        
+        oc_pwm(&oc8, &D[8], &timer3, freq, duty); 
+        oc_pwm(&oc8, &D[6], &timer3, freq, duty);     
     }
 }
