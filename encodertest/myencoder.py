5c6df84c8ebdad4529ac0531841489322df984f7
amuthaOlin <ankeet.mutha@students.olin.edu>
2016-02-05 17:48:20 -0500

    Encoder reading fully functional

--- encodertest/myencoder.py
+++ encodertest/myencoder.py
@@ -15,19 +15,18 @@ while(1):
 #	mag = bin(mag)[2:]
 #	print mag
 
-	angle = myencoder.enc_readReg(ENC_ANGLE)
-	angle = struct.unpack('<H',angle)[0]
+	pack = myencoder.enc_readReg(ENC_ANGLE)
+	origAngle = struct.unpack('<H',pack)[0]
 
 	
 
-	binAngleOrig = bin(angle)
+	binAngleOrig = format(origAngle, '#018b')
 	binAngle = binAngleOrig[4:]
-	binAngle.zfill(14)
 	intAngle = int(binAngle,2)
 	#print "int = ",angle
 	angle = (intAngle*360.0)/(2**14)
 	if binAngleOrig[3] == "0":
-		print "angle = ", angle, "  intAngle = ", intAngle, "   binAngle = ", binAngle, "   binAngleTruncated = ",binAngle[4:]   binAngleOrig = ", binAngleOrig
+		print "angle = ", angle, "  intAngle = ", intAngle, "   binAngle = ", binAngle, "   binAngleTruncated = ", binAngleOrig[4:], "   binAngleOrig = ", binAngleOrig, "   origAngle = ", origAngle 
 	else:
 		print "error"
 		#print "angle = ", angle, "  intAngle = ", intAngle, "   binAngle = ", binAngle, "   binAngleOrig = ", binAngleOrig
