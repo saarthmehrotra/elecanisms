a779bc8b844f00c518822dc924e3f5648b0e8c0e
amuthaOlin <ankeet.mutha@students.olin.edu>
2016-02-08 08:55:39 -0500

    got hello working for communication with screen

--- hello/hello.c
+++ hello/hello.c
@@ -2,6 +2,7 @@
 #include "config.h"
 #include "common.h"
 #include "uart.h"
+#include "timer.h"
 #include <stdio.h>
 
 uint8_t string[40];
@@ -9,6 +10,10 @@ uint8_t string[40];
 int16_t main(void) {
     init_clock();
     init_uart();
+    init_timer();
+
+    timer_setPeriod(&timer2, 1.25);
+    timer_start(&timer2);
 
     printf("Hello World!\n");
 
@@ -21,7 +26,8 @@ int16_t main(void) {
     while (1) {
         printf(">> ");
         uart_gets(&uart1, string, 40);
-        printf("You typed '%s'\n", string);
+        printf("You typed '%s' and the timer period is %d \n", string, timer_period(&timer2));
+
     }
 }
 
