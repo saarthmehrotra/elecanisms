328e8d02e1f00686ea94390cf04633cb999a2357
amuthaOlin <ankeet.mutha@students.olin.edu>
2016-02-08 08:56:32 -0500

    used blink to get 2 way communication. Something wrong with timers.

--- blink/blink.c
+++ blink/blink.c
@@ -1,17 +1,21 @@
 #include <p24FJ128GB206.h>
+#include <stdio.h>
 #include "config.h"
 #include "common.h"
 #include "ui.h"
 #include "timer.h"
+#include "uart.h"
 
 int16_t main(void) {
     init_clock();
     init_ui();
     init_timer();
+    init_uart();
+
 
     led_on(&led1);
     led_off(&led2);
-    timer_setPeriod(&timer2, 0.5);
+    timer_setPeriod(&timer2, 1.5e1);
     timer_start(&timer2);
 
     while (1) {
@@ -20,6 +24,7 @@ int16_t main(void) {
             led_toggle(&led1);
             led_toggle(&led2);
         }
+        printf("%s\n", timer_period(&timer2));
 
 
         led_write(&led3, !sw_read(&sw3));
